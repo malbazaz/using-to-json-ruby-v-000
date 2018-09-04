ceece2c4a76fc44671a72c85d3a861a51ea19ed9
Mo Al <malbazaz@mba2018.hbs.edu>
2018-09-04 19:59:06 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
     respond_to do |format|
       format.html { render :show }
       format.json { render json: @post.to_json(only: [:title, :description, :id], include: [author: { only: [:name]}]) }
-      post.to_json(include: :author)
+      # post.to_json(include: :author)
     end
   end
 
