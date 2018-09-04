90fe371247da2266524c926bba6c93935bb8a486
Mo Al <malbazaz@mba2018.hbs.edu>
2018-09-04 20:04:06 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit]
   get 'posts/:id/post_data', to: 'posts#post_data'
-  get 'posts/:id', to: 'posts#post_data'
   get 'posts/:id', to: 'posts#show'
   post 'posts/:id', to: 'posts#show'
 end
